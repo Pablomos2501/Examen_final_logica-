7402f226baca2e5ddfc0f5df0253fa15996c835f
pablo <remagforce@gmail.com>
2019-06-01 08:55:58 -0600

    comentarios

--- examen.py
+++ examen.py
@@ -5,8 +5,7 @@ Pablo Otoniel Moscoso Flores
 0907-19-2477
 """
 from tkinter import *
-from tkinter import messagebox
-
+#funcion del primer boton 
 def calcular():
     if v1.get() < v3.get():
         messagebox.showinfo("multiplicar",f" se esta multiplicando   y la respuesta es   {v1.get() * v2.get() * v3.get() }   ")
@@ -16,21 +15,18 @@ def calcular():
         messagebox.showinfo("restando",f" se esta restando   y la respuesta es   {v1.get() - v3.get() }  ")
     if v2.get() == 0 and v3.get() < v1.get():
         messagebox.showinfo("restando",f" se esta restando   y la respuesta es   {v3.get() - v1.get() }   ")
-    
+#funcion del segundo boton 
 def concatenar():
     for x in 0:
         if v1.get()  v2.get():
             messagebox.showinfo("multiplicar",f" se esta multiplicando   y la respuesta es   {v1.get() * v2.get() + v3.get() }   ")
 
-
-
-
-
-
+#interfaz
 interfaz=Tk()
 interfaz.geometry("500x300+100+100")
 interfaz.title("examen")
 
+#labeles y entradas 
 lbl=Label(text="valor 1").place(x=10,y=40)
 v1=IntVar()
 ingreso=Entry(interfaz,textvariable=v1).place(x=60,y=40)
@@ -42,7 +38,7 @@ ingreso=Entry(interfaz,textvariable=v2).place(x=60,y=90)
 lbl3=Label(text="valor 3").place(x=10,y=120)
 v3=IntVar()
 ingreso=Entry(interfaz,textvariable=v3).place(x=60,y=120)
-
+#botones
 boton=Button(interfaz,text="calcular",command=calcular).place(x=10,y=150)
 boton2=Button(interfaz,text="concatenar", command=concatenar).place(x=10,y=180)
 
